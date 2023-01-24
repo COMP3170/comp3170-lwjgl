673cd8c291014d420111423311ce166f88c7cba3
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-01-24 15:23:50 +1100

    Added capabilities to init()

--- src/comp3170/IWindowListener.java
+++ src/comp3170/IWindowListener.java
@@ -1,13 +1,32 @@
 package comp3170;
 
+import org.lwjgl.opengl.GLCapabilities;
+
 public interface IWindowListener {
 
-	public void init();
+	/**
+	 * Initialise the scene. Called once when the Window is first created
+	 * @param capabilities The capabilities for the current GL implementation
+	 */
+	
+	public void init(GLCapabilities capabilities);
+
+	/**
+	 * Draw the scene. Called once every frame.
+	 */
 
 	public void draw();
 
+	/**
+	 * Reshape the canvas. Called when the window has been resized.
+	 */
+
 	public void reshape();
 	
+	/**
+	 * Close the scene. Called when the window is closed.
+	 */
+
 	public void close();
 
 }
