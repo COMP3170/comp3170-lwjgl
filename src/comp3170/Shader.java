9036a3bbb4d9cdddfc6aedf680503f111b31cc6f
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-01-24 17:35:39 +1100

    Record attributes and uniforms.

--- src/comp3170/Shader.java
+++ src/comp3170/Shader.java
@@ -1,16 +1,27 @@
 package comp3170;
 
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.system.MemoryStack.*;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-import static org.lwjgl.opengl.GL41.*;
+import org.lwjgl.system.MemoryStack;
 
 public class Shader {
 
 	private int program;
+	private HashMap<String, Integer> attributes;
+	private HashMap<String, Integer> attributeTypes;
+	private HashMap<String, Boolean> trackedAttributeErrors;
+	private HashMap<String, Integer> uniforms;
+	private HashMap<String, Integer> uniformTypes;
+	private HashMap<String, Boolean> trackedUniformErrors;
 
 	public Shader(File vertexShaderFile, File fragmentShaderFile) throws IOException, GLException {
 
@@ -36,9 +47,13 @@ public class Shader {
 
 		// record attribute and uniforms
 
-//		recordAttributes();
-//		recordUniforms();
+		recordAttributes();
+		recordUniforms();
 	}
+	
+	//
+	// Shader compilation
+	//
 
 	/**
 	 * Read source code from a shader file.
@@ -119,6 +134,64 @@ public class Shader {
 		return program;
 	}
 
+	//
+	// Uniforms and Attributes
+	// 
+	
+	/**
+	 * Establish the mapping from attribute names to IDs
+	 */
+
+	private void recordAttributes() {
+		attributes = new HashMap<String, Integer>();
+		attributeTypes = new HashMap<String, Integer>();
+		trackedAttributeErrors = new HashMap<String, Boolean>();
+
+		int activeAttributes = glGetProgrami(program, GL_ACTIVE_ATTRIBUTES);		
+		int maxNameSize = glGetProgrami(program, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH);
+
+		// Use stack buffers for "any small buffer/struct allocation that is shortly-lived"
+		// https://blog.lwjgl.org/memory-management-in-lwjgl-3/
+		MemoryStack stack = stackPush();
+		IntBuffer sizeBuffer = stack.mallocInt(1);
+		IntBuffer typeBuffer = stack.mallocInt(1);
+
+		for (int i = 0; i < activeAttributes; ++i) {
+			String name = glGetActiveAttrib(program, i, maxNameSize, sizeBuffer, typeBuffer);
+
+			attributes.put(name, glGetAttribLocation(program, name));
+			attributeTypes.put(name, typeBuffer.get(0));
+		}
+	}
+
+	/**
+	 * Establish the mapping from uniform names to IDs
+	 * @param trackedUniformErrors 
+	 */
+
+	private void recordUniforms() {
+		uniforms = new HashMap<String, Integer>();
+		uniformTypes = new HashMap<String, Integer>();
+		trackedUniformErrors = new HashMap<String, Boolean>();
+
+		int activeUniforms = glGetProgrami(program, GL_ACTIVE_UNIFORMS);
+		int maxNameSize = glGetProgrami(program, GL_ACTIVE_UNIFORM_MAX_LENGTH);
+
+		// Use stack buffers for "any small buffer/struct allocation that is shortly-lived"
+		// https://blog.lwjgl.org/memory-management-in-lwjgl-3/
+		MemoryStack stack = stackPush();
+		IntBuffer sizeBuffer = stack.mallocInt(1);
+		IntBuffer typeBuffer = stack.mallocInt(1);
+
+		for (int i = 0; i < activeUniforms; ++i) {
+			String name = glGetActiveUniform(program, i, maxNameSize, sizeBuffer, typeBuffer);
+
+			uniforms.put(name, glGetUniformLocation(program, name));
+			uniformTypes.put(name, typeBuffer.get(0));
+		}
+	}
+
+	
 	/**
 	 * Turn a shader type constant into a descriptive string.
 	 * 
