16d611cdcb063cec3f824294aa7aecff7271f2f3
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-04-22 14:37:46 +1000

    Implemented multipass rendering

--- src/comp3170/SceneObject.java
+++ src/comp3170/SceneObject.java
@@ -12,7 +12,7 @@ import org.joml.Matrix4f;
  * 
  * 1) Create a 'root' object in your scene:
  * 
- * root = new SceneObject();
+ *    root = new SceneObject();
  * 
  * 2) Extend this class to implement specific objects in the graph. 
  * 
@@ -21,22 +21,51 @@ import org.joml.Matrix4f;
  *    
  * 4) Add new objects to the graph using the setParent method, e.g.:
  * 
- * Ship ship = new Ship(); // a subclass of SceneObject
- * ship.setParent(root);   // ship is now a child of root in the graph
+ *    Ship ship = new Ship(); // a subclass of SceneObject
+ *    ship.setParent(root);   // ship is now a child of root in the graph
  * 
- * Pirate pirate = new Pirate(): // a subclass of SceneObject
- * private.setParent(ship);		 // pirate is now a child of ship 	
+ *    Pirate pirate = new Pirate(): // a subclass of SceneObject
+ *    private.setParent(ship);		 // pirate is now a child of ship 	
  * 
- * 5) You can use this class without extending it to make 'pivot' objects in the graph that are not drawn.
+ * 5) You can use this class without extending it to make 'pivot' objects in 
+ *    the graph that are not drawn.
  * 
- * 6) Use the getMatrix() method to access the local coordinate frame matrix from this object to its parent.
+ * 6) Use the getMatrix() method to access the local coordinate frame matrix 
+ *    from this object to its parent.
  * 
- * Matrix4f pirateToShipMatrix = pirate.getMatrix();
- * pirateToShipMatrix.translate(1,0,0);		// move the pirate 1 unit to the right in its local coordinate frame
+ *    Matrix4f pirateToShipMatrix = pirate.getMatrix();
+ *    pirateToShipMatrix.translate(1,0,0);	// move the pirate 1 unit to the right 
  * 
  * 7) Use the draw() method to draw the entire scene graph with a given shader. 
  * 
- * root.draw(); // recursively draws the ship and the pirate 
+ *    root.draw(); // recursively draws the ship and the pirate 
+ * 
+ * 8) If you are using a camera, you can pass in the view-projection matrix at the root
+ *    and calculate the model-view-projection (MVP) matrix recursively as you descend
+ *    the graph/
+ * 
+ *    projectionMatrix.mul(viewMatrix, mvpMatrix); // MVP = MP * MV
+ *    root.draw(mvpMatrix);
+ *    
+ * 9) If you are implementing multipass rendering, you can also include a 'pass' value, e.g.
+ *  
+ *    // PASS 0: Render opaque geometry
+ *    root.draw(mvpMatrix, 0);
+ *    
+ *    // PASS 1: Render transparent geometry
+ *    root.draw(mvpMatrix, 1);
+ * 
+ *    Then override drawSelf(matrix, pass) to do different things on each pass, e.g.
+ *    
+ *    void drawSelf(Matrix4f matrix, int pass) {
+ *    	if (pass == 0) {
+ *      	// OPAQUE PASS
+ *   	}
+ *   	else if (pass == 1) {
+ *   		// TRANSPARENT PASS
+ *   	}
+ *    }
+ * 
  * 
  * @author malcolmryan
  *
@@ -132,6 +161,20 @@ public class SceneObject {
 	}
 
 	/**
+	 * Draw this object. Override this in subclasses to draw specific objects.
+	 * Override this method to implement multipass rendering. 
+	 * 
+	 * By default it calls drawSelf on the pass 0 and does nothing on later passes.
+	 * 
+	 * @param matrix	The matrix to use.
+	 */
+	protected void drawSelf(Matrix4f matrix, int pass) {
+		if (pass == 0) {
+			drawSelf(matrix);			
+		}
+	}
+	
+	/**
 	 * Draw this object and all its children in the subgraph.
 	 * 
 	 */
@@ -146,20 +189,31 @@ public class SceneObject {
 	/**
 	 * Draw this object and all its children in the subgraph.
 	 * 
-	 * @param shader	The shader to use.
 	 * @param parentMatrix	The model->world matrix for the parent. Set to the identity matrix when drawing the root.
 	 */
 	public void draw(Matrix4f parentMatrix) {
+		// draw everything on pass 0
+		draw(parentMatrix, 0);
+	}
+
+	/**
+	 * Draw this object and all its children in the subgraph.
+	 * Use this version for multipass rendering
+	 * 
+	 * @param parentMatrix	The model->world matrix for the parent. Set to the identity matrix when drawing the root.
+	 * @param pass	The number of this draw pass (default 0)
+	 */
+	public void draw(Matrix4f parentMatrix, int pass) {
 		// W = P * M
 		tempModelMatrix.set(parentMatrix);			// W = P
 		tempModelMatrix.mul(modelToParentMatrix);	// W = W * M
 		
 		// draw the object using the shader and calculated model matrix
-		drawSelf(tempModelMatrix);
+		drawSelf(tempModelMatrix, pass);
 	
 		// recursively draw the children
 		for (SceneObject child : children) {
-			child.draw(tempModelMatrix);
+			child.draw(tempModelMatrix, pass);
 		}
 		
 	}
